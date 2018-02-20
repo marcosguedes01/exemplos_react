7dffea082fa9574dce48d39c1044377be9367a32
Marcos Guedes <marcosguedes01@gmail.com>
2018-02-20 08:16:35 -0300

    arquivos iniciais

---
+++ ex/family.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default props => (
+    <div>
+        <h1>Família</h1>
+        { React.Children.map(
+            props.children, 
+            child => React.cloneElement(child, {...props})
+            )}
+    </div>
+)
